0c34beb54c3ea6ef3e2d1f3d44b8b85849c3c1c0
Ofoefule Christian <ofoefule.c@gmail.com>
2022-01-02 16:55:17 +0100

    implemented moderation service and event syncing

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,14 +7,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  events.push(event);
+
+  try {
+    axios.post("http://localhost:4000/events", event);
+    axios.post("http://localhost:4001/events", event);
+    axios.post("http://localhost:4002/events", event).catch((err) => err);
+    axios.post("http://localhost:4003/events", event);
+  } catch (error) {}
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
