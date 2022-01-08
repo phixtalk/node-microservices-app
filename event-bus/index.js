6b610dc95b0ffb5bb663d21ba4470f9698c8ed2c
Ifeanyi Ofoefule <ofoefulec_fny@yahoo.com>
2022-01-08 07:45:06 -0800

    implemented deployments and cluster services config for comments, moderation and query, updated index files to point to cluster id services url

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,11 +16,12 @@ app.post("/events", (req, res) => {
 
   try {
     axios.post("http://posts-clusterip-srv:4000/events", event);
-    // axios.post("http://localhost:4001/events", event);
-    // axios.post("http://localhost:4002/events", event).catch((err) => err);
-    // axios.post("http://localhost:4003/events", event);
+    axios.post("http://comments-srv:4001/events", event);
+    axios.post("http://query-srv:4002/events", event).catch((err) => err);
+    axios.post("http://moderation-srv:4003/events", event);
   } catch (error) {}
 
+  console.log("Events received");
   res.send({ status: "OK" });
 });
 
